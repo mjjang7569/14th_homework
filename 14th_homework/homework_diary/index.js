739a0aaf0edafbe425a90881278aea0709b057f0
mjjang7569 <mjjang7569@gmail.com>
2025-08-12 00:09:01 +0900

    1. 필터기능, 2. 새로고침시 ui없어지는 현상해결

--- 14th_homework/homework_diary/index.js
+++ 14th_homework/homework_diary/index.js
@@ -1,6 +1,6 @@
-let i = 0
+
+
 let 카드 = document.getElementById("카드영역")
-let 일기카드배열 = []
 let 단일카드
 
 
@@ -34,65 +34,174 @@ const 일기카드목록 = () =>{
         제목 : 오늘의제목,
         내용 : 오늘의내용
     } 
-    console.log(단일카드)
+    let 일기카드배열 = JSON.parse(localStorage.getItem("일기카드들")) || []
     일기카드배열.push(단일카드)
     let 카드저장 = localStorage.setItem("일기카드들", JSON.stringify(일기카드배열))
     let 카드내용 = JSON.parse(localStorage.getItem("일기카드들"))
     console.log("로컬저장후 불러옴", 카드내용, typeof(카드내용))
-    // 카드.innerHTML = 카드배열.map((ele, index)=>{
-    //     `       <a href="./detial.html?number=${i}">
-    //             <div id="카드${i}" class="카드">
-    //                     <div id="카드이미지${i}" class="카드이미지"></div>
-    //                     <div id="" class="카드내용요약">
-    //                         <div class="첫줄">
-    //                             <div class="기분노출">
-    //                                 <div>${ele.기분}</div>
-    //                             </div>
-    //                             <div class="날짜노출">'00.00.00'</div>
-    //                         </div>
-    //                         <div class="내용요약">
-    //                             <div>${ele.내용}</div>
-    //                         </div>                     
-    //                 </div>
-    //                 </div>
-    //             </a>
-    // `
-    // })
-    카드.innerHTML = 카드.innerHTML +
+    let 이미지경로 = (ele, index)=>{
+        
+            if(ele.기분 === "행복해요"){
+                image_url = './assets/Frame4.png'
+            }else if(ele.기분 === "슬퍼요"){
+                image_url = './assets/Frame1.png'
+            }else if(ele.기분 === "놀랐어요"){
+                image_url = './assets/Frame2.png'
+            }else if(ele.기분 === "화나요"){
+                image_url = './assets/Frame3.png'
+            }else if(ele.기분 === "기타"){
+                image_url = './assets/Frame5.png'
+            }
+        return image_url
+        
+    }
+    let 경로 = 카드내용.map(이미지경로)
 
-    `       <a href="./detial.html?number=${i}">
-                <div id="카드${i}" class="카드">
-                        <div id="카드이미지${i}" class="카드이미지"></div>
-                        <div id="" class="카드내용요약">
-                            <div class="첫줄">
-                                <div class="기분노출">
-                                    <div>${카드내용[i].기분}</div>
+    let 태그생성 = (ele, index)=>{
+        let image_url_=경로[index]
+        return    `<a href="./detial.html?number=${index}">
+                    <div id="카드${index}" class="카드">
+                            <div id="카드이미지${index}" class="카드이미지" style="background-image: url(${image_url_})">
+                                <button class="삭제버튼" onclick='카드삭제기능()'>X</button>
+                            </div>                            
+                            <div id="" class="카드내용요약">
+                                <div class="첫줄">
+                                    <div class="기분노출">
+                                        <div>${ele.기분}</div>
+                                    </div>
+                                    <div class="날짜노출">'00.00.00'</div>
                                 </div>
-                                <div class="날짜노출">'00.00.00'</div>
-                            </div>
-                            <div class="내용요약">
-                                <div>${카드내용[i].내용}</div>
-                            </div>                     
-                    </div>
+                                <div class="내용요약">
+                                    <div>${ele.내용}</div>
+                                </div>                     
+                        </div>
                     </div>
                 </a>
-    `
-    if(행복){
-       document.getElementById(`카드이미지${i}`).style.backgroundImage = "url('./assets/Frame4.png')"
-    }else if(슬픔){
-        image_url = "./assets/Frame1.png"
-        document.getElementById(`카드이미지${i}`).style.backgroundImage = "url('./assets/Frame1.png')"  
-    }else if(놀람){
-        image_url = "./assets/Frame2.png"
-        document.getElementById(`카드이미지${i}`).style.backgroundImage = "url('./assets/Frame2.png')"
-    }else if(화남){
-        image_url = "./assets/Frame3.png"
-        document.getElementById(`카드이미지${i}`).style.backgroundImage = "url('./assets/Frame3.png')"
-    }else if(기타){
-        image_url = "./assets/Frame5.png"
-        document.getElementById(`카드이미지${i}`).style.backgroundImage = "url('./assets/Frame5.png')" 
+                `
     }
-
-    i+=1  
+    카드.innerHTML = 카드내용.map(태그생성).join("")
+    console.log(카드.innerHTML)  
     
+}
+
+const 필터기능 = (event)=>{
+    let 필터링전모든카드= JSON.parse(localStorage.getItem("일기카드들")) || []
+    let 필터된카드 = localStorage.setItem("필터링일기카드들", JSON.stringify(필터링전모든카드))
+    필터링전모든카드= JSON.parse(localStorage.getItem("일기카드들")) 
+    console.log("필터링전모든카드 : ", 필터링전모든카드)
+    const 선택된감정 = event.target.value
+    console.log("선택된감정 : ", 선택된감정)
+    let 필터된일기
+    switch(선택된감정){
+        case "행복" :{
+            필터된일기 = 필터링전모든카드.filter((ele) => ele.기분 === "행복해요")
+            console.log("필터된일기 :", 필터된일기)
+            url_='./assets/Frame4.png'
+            break
+        }
+        case "슬픔" :{
+            필터된일기 = 필터링전모든카드.filter((ele) => ele.기분 === "슬퍼요")
+            console.log("필터된일기 :", 필터된일기)
+            url_='./assets/Frame1.png'
+            break
+        }
+        case "놀람" :{
+            필터된일기 = 필터링전모든카드.filter((ele) => ele.기분 === "놀랐어요")
+            console.log("필터된일기 :", 필터된일기)
+            url_='./assets/Frame2.png'
+            break
+        }
+        case "화남" :{
+            필터된일기 = 필터링전모든카드.filter((ele) => ele.기분 === "화나요")
+            console.log("필터된일기 :", 필터된일기)
+            url_='./assets/Frame3.png'
+            break
+        }
+        case "기타" :{
+            필터된일기 = 필터링전모든카드.filter((ele) => ele.기분 === "기타")
+            console.log("필터된일기 :", 필터된일기)
+            url_='./assets/Frame5.png'
+            break
+        }
+        case "전체" : {
+            JSON.parse(localStorage.getItem("일기카드들"))
+            일기카드목록()
+            return
+        }
+    }
+    console.log("필터된일기 : ", 필터된일기)
+    카드.innerHTML = ""
+    카드.innerHTML = 필터된일기.map((ele, index)=>{
+        let original_index = 필터링전모든카드.indexOf(ele)
+
+        return `<a href="./detial.html?number=${original_index}">
+                    <div id="카드${index}" class="카드">
+                            <div id="카드이미지${index}" class="카드이미지" style="background-image: url(${url_})">
+                                <button class="삭제버튼" onclick='카드삭제기능(${index})'>X</button>
+                            </div>
+                            <div id="" class="카드내용요약">
+                                <div class="첫줄">
+                                    <div class="기분노출">
+                                        <div>${ele.기분}</div>
+                                    </div>
+                                    <div class="날짜노출">'00.00.00'</div>
+                                </div>
+                                <div class="내용요약">
+                                    <div>${ele.내용}</div>
+                                </div>                     
+                        </div>
+                    </div>
+                </a>
+                `
+    }).join("")
+        
+    // return 필터된일기
+
+}
+const 카드삭제기능 = (index) =>{
+    JSON.parse(localStorage.getItem("일기카드들").removeItem(index))
+}
+
+
+window.onload = () =>{
+    let 카드목록 = JSON.parse(localStorage.getItem("일기카드들"))
+    let 이미지경로 = (ele)=>{
+        
+        if(ele.기분 === "행복해요"){
+            image_url = './assets/Frame4.png'
+        }else if(ele.기분 === "슬퍼요"){
+            image_url = './assets/Frame1.png'
+        }else if(ele.기분 === "놀랐어요"){
+            image_url = './assets/Frame2.png'
+        }else if(ele.기분 === "화나요"){
+            image_url = './assets/Frame3.png'
+        }else if(ele.기분 === "기타"){
+            image_url = './assets/Frame5.png'
+        }
+        return image_url
+    
+    }
+    카드.innerHTML = 카드목록.map((ele, index) => {
+        let image_url = 이미지경로(ele);
+        return `
+            <a href="./detial.html?number=${index}">
+                <div id="카드${index}" class="카드">
+                    <div id="카드이미지${index}" class="카드이미지" style="background-image: url(${image_url})">
+                        <button class="삭제버튼" onclick='카드삭제기능(${index})'>X</button>
+                    </div>
+                    <div class="카드내용요약">
+                        <div class="첫줄">
+                            <div class="기분노출">
+                                <div>${ele.기분}</div>
+                            </div>
+                            <div class="날짜노출">'00.00.00'</div>
+                        </div>
+                        <div class="내용요약">
+                            <div>${ele.내용}</div>
+                        </div>                     
+                    </div>
+                </div>
+            </a>
+        `;
+    }).join("")
 }
